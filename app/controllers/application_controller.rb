d9ff28217de1a37953f4c78d8de83eb9d1e32e04
Abhishek Satpathy <abhisheksatpathy28@gmail.com>
2017-12-13 12:03:46 +1100

    Chart Working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
+require 'elasticsearch'
+require 'json'
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+
+  def elastic_test
+    client = Elasticsearch::Client.new transport_options: {
+        request: { timeout: 10000 },
+    }, retry_on_failure: true, hosts:[
+        { host: 'test.es.streem.com.au',
+          port: '9200',
+          user: 'elastic',
+          password: 'streem'
+
+        }], log: true, reload_connections: true
+
+    begin
+        data = JSON.parse(request.body.string)
+        p(data['urls'])
+        client.cluster.health
+        i = 0
+        response = Array.new
+        while i<data['urls'].length do
+          response[i] = client.search q: data['urls'][i]
+          i+=1
+        end
+        render plain: response
+    rescue
+      render :elastic_test
+    end
+  end
 end
+
